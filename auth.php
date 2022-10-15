a49e4c27e509f1452cf269f2f33e896946826e79
Investor_v_slotovuy <koenigsegg970@mail.ru>
2022-10-15 16:30:54 +0400

    Четвертая версия

--- auth.php
+++ auth.php
@@ -7,15 +7,14 @@ $sql = pg_connect("host=localhost port=5432 dbname=postgres user=postgres passwo
 
 $result = pg_query($sql, "SELECT login, password FROM first_practice WHERE login = '$login' AND password = '$pass'");
 $user = pg_fetch_row($result); // Конвертируем в массив
-if(empty($user)) {
-    echo "Пользователь не найден.";
-    exit();
-}
+        if (empty($user)) {
+            echo "Пользователь не найден.";
+            exit();
+        }
+        setcookie('user', $user['name'], time() + 3600, "/");
 
-setcookie('user', $user['name'], time() + 3600, "/");
+        pg_close($sql);
 
-pg_close($sql);
-
-header('Location: page.html');
+        header('Location: page.html');
 
 
